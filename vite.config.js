56c4cbee27abf6c057a206a1ed1d5d075c538114
viss5vuus98 <dokipu@gmailcom>
2023-07-16 17:13:50 +0800

    調整圖片路徑

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
-import { defineConfig } from 'vite';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { glob } from 'glob';
-
-export default defineConfig({
+export default ({
   base: '/Personal_Page/',
   server: {
     open: '/index.html',
