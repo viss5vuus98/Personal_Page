0d5d004670ab244a6126933334b9aefd1ccf849c
viss5vuus98 <dokipu@gmailcom>
2023-07-16 13:50:47 +0800

    修改vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-export default ({
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { glob } from 'glob';
+
+export default defineConfig({
+  base: '/Personal_Page/',
   server: {
     open: '/index.html',
   },
   build: {
-    base: '/Personal_Page/',
     outDir: 'dist',
   }
 })
